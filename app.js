1b86b6a7dd294d9f3fd124af324938648da8b32c
noyzilla <noyzilla@users.noreply.github.com>
2024-05-02 09:08:22 +0700

    support multi ports

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
 require('dotenv').config();
+
 const ProxyChain = require('proxy-chain');
 
 const CONFIG = {
-    PORT: process.env.PORT || 8000,
+    PORTS: Array.from(
+        new String(process.env.PORTS || "8000").split(/,/).map((p) => {
+            if (p.indexOf('-') > 0) {
+                const [start, end] = p.split(/-/).map((v) => parseInt(v));
+                return Array.from({length: end - start + 1}, (v, k) => start + k);
+            }
+            return p;
+        }).flat()
+    ),
     UPSTREAM: process.env.UPSTREAM || null,
     VERBOSE: process.env.VERBOSE || false,
-    STIME: new Date().getTime(),
+    TIME: new Date().getTime(),
 };
 
-const server = new ProxyChain.Server({
-    port: CONFIG.PORT,
-    verbose: CONFIG.VERBOSE,
-    prepareRequestFunction: ({username, password}) => {
-        let upstreamProxyUrl = CONFIG.UPSTREAM;
-        if (upstreamProxyUrl) {
-            upstreamProxyUrl = upstreamProxyUrl.replace(/<username>/, username);
-            upstreamProxyUrl = upstreamProxyUrl.replace(/<random>/, String(Math.random()));;
-            upstreamProxyUrl = upstreamProxyUrl.replace(/<stime>/, CONFIG.STIME);
-        }
-        return {
-            upstreamProxyUrl: upstreamProxyUrl
-        };
-    },
-});
+CONFIG.PORTS.forEach(key_port => {
+    const server = new ProxyChain.Server({
+        port: cport,
+        verbose: CONFIG.VERBOSE,
+        prepareRequestFunction: ({username, password}) => {
+            let upstreamProxyUrl = CONFIG.UPSTREAM;
+            if (upstreamProxyUrl) {
+                upstreamProxyUrl = upstreamProxyUrl.replace(/<cport>/, cport);
+                upstreamProxyUrl = upstreamProxyUrl.replace(/<ctime>/, CONFIG.TIME);
+                upstreamProxyUrl = upstreamProxyUrl.replace(/<random>/, String(Math.random()));
+                upstreamProxyUrl = upstreamProxyUrl.replace(/<username>/, username);
+            }
+            return {
+                upstreamProxyUrl: upstreamProxyUrl
+            };
+        },
+    });
 
-server.listen(() => {
-    console.log(`Proxy server is listening on port ${server.port}`);
-});
+    server.listen(() => {
+        console.log(`Proxy server is listening on port ${server.port}`);
+    });
 
-// Emitted when HTTP connection is closed
-server.on('connectionClosed', ({connectionId, stats}) => {
-    console.log(`Connection ${connectionId} closed`);
-    console.dir(stats);
-});
+    // Emitted when HTTP connection is closed
+    server.on('connectionClosed', ({connectionId, stats}) => {
+        console.log(`Connection ${connectionId} closed`);
+        console.dir(stats);
+    });
 
-// Emitted when HTTP request fails
-server.on('requestFailed', ({request, error}) => {
-    console.log(`Request ${request.url} failed`);
-    console.error(error);
+    // Emitted when HTTP request fails
+    server.on('requestFailed', ({request, error}) => {
+        console.log(`Request ${request.url} failed`);
+        console.error(error);
+    });
 });
